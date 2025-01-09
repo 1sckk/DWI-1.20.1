3ac28a6b846911069571afa68954f0ef7621383d
Notgorgo <georgeschlmmer10@gmail.com>
2025-01-08 20:33:19 -0500

    Added Rubys

---
+++ src/main/java/xyz/notgorgo/dwi/item/ModItems.java
@@ -0,0 +1,34 @@
+package xyz.notgorgo.dwi.item;
+
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import xyz.notgorgo.DWI;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+    entries.add(ModItems.RUBY);
+    entries.add(ModItems.RAW_RUBY);
+}
+
+
+
+    private  static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(DWI.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        DWI.LOGGER.info("Registering Mod Items for" + DWI.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}

b6f08a459053c5d0d40a63b7943041161581df25
Notgorgo <georgeschlmmer10@gmail.com>
2025-01-09 19:35:10 -0500

    Added custom tags for dev and added some data gen code Done!

--- src/main/java/xyz/notgorgo/dwi/datagen/ModRecipeProvider.java
+++ src/main/java/xyz/notgorgo/dwi/datagen/ModRecipeProvider.java
@@ -1,18 +1,45 @@
 package xyz.notgorgo.dwi.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.registry.RegistryWrapper;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import xyz.notgorgo.dwi.block.ModBlocks;
+import xyz.notgorgo.dwi.item.ModItems;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.List;
+import java.util.function.Consumer;
 
-public class ModRecipeProvider extends FabricTagProvider.BlockTagProvider {
-    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-        super(output, registriesFuture);
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static final List<ItemConvertible> RUBY_SMELTABLES = List.of(ModItems.RAW_RUBY,
+            ModBlocks.RUBY_ORE, ModBlocks.DEEPSLATE_RUBY_ORE, ModBlocks.NETHER_RUBY_ORE, ModBlocks.END_STONE_RUBY_ORE);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
     }
 
     @Override
-    protected void configure(RegistryWrapper.WrapperLookup arg) {
-        getOrCreateTagBuilder(null);
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 100, "ruby");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RUBY, RecipeCategory.MISC,
+                ModBlocks.RUBY_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RUBY, 1)
+                .pattern("RRR")
+                .pattern("RSR")
+                .pattern("RRR")
+                .input('R', ModItems.RUBY)
+                .input('S', Items.STONE)
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .offerTo(exporter,new Identifier(getRecipeName(ModItems.RAW_RUBY)));
+
     }
 }
